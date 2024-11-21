a30a93817a6d00a7b8dea40f463b6307c7a18b39
Kyle Wadas <wadaskyle03@gmail.com>
2024-11-20 18:34:10 -0700

    Tower Placer Shtuff

---
+++ Assets/Game/KyleScene/Scripts/Tutorial/TowerPlacer.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class TowerPlacer : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    private GameObject currentMonke;
+
+    void Update()
+    {
+        if(currentMonke != null)
+        {
+            Ray camray = cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(camray, out RaycastHit hitInfo, 100f))
+            {
+                if (hitInfo.collider.tag == "Ground")
+                {
+                    currentMonke.transform.position = new Vector3(hitInfo.point.x, currentMonke.transform.position.y, hitInfo.point.z);
+                }
+            }
+
+            //print(hitInfo.collider.tag);
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                currentMonke = null;
+            }
+        }
+    }
+
+    public void SetTowerToPlace(GameObject tower)
+    {
+        currentMonke = Instantiate(tower, Vector3.zero, Quaternion.identity);
+    }
+}
+
+/*
+    public Color hoverColor;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+*/
