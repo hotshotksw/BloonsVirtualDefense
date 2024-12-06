83146deb69c94b69cd48373283f791ac60e8fae3
JohnBeck19 <jbeck@student.neumont.edu>
2024-12-05 18:02:42 -0700

    dart spawner

--- Assets/Game/Scripts/DartSpanwer.cs
+++ Assets/Game/Scripts/DartSpanwer.cs
@@ -22,7 +22,7 @@ public class DartSpanwer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        spawnPoint = GameObject.Find("DartSpanwer").transform;
+        spawnPoint = GameObject.Find("DartSpawner").transform;
     }
 
     // Update is called once per frame
